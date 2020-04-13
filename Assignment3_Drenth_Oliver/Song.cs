e744664e2bdd7d25cd325f7981d7eff1eb666b4c
Oliver Drenth <oliverdrenth@gmail.com>
2020-04-12 20:09:15 -0400

    Add project files.

---
+++ Assignment3_Drenth_Oliver/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_Drenth_Oliver
+{
+    class Song
+    {
+        public string ArtistName;
+        public string SongName;
+        public int SongLegnth;
+    }
+}
